349fcea3805ffe513d32f6a4894ccdfdf44e5fc0
Marc Tönsing <marc.toensing@gmail.com>
2017-03-01 18:07:59 +0100

    fixed css bug

--- shortscore-rangeslider.php
+++ shortscore-rangeslider.php
@@ -4,7 +4,7 @@
   Plugin Name: SHORTSCORE Rangeslider
   Plugin URI: http://www.shortscore.org
   Description: Frontend slider for the shortscore dropdown.
-  Version: 1.1
+  Version: 1.2
   Author: MarcDK
   Author URI: http://www.marc.tv
   GitHub Plugin URI: mtoensing/wp-shortscore-rangeslider
@@ -15,13 +15,13 @@ function add_shortscore_rangeslider_scripts()
 {
     if (is_user_logged_in()) {
         wp_enqueue_style(
-            "jquery.shortscorerangeslider.css", WP_PLUGIN_URL . "/wp-shortscore-rangeslider/rangeslider.css", false, "1.1");
+            "jquery.shortscorerangeslider.css", WP_PLUGIN_URL . "/wp-shortscore-rangeslider/rangeslider.css", false, "1.2");
 
         wp_enqueue_script(
-            "jquery.shortscorerangeslider.js", WP_PLUGIN_URL . "/wp-shortscore-rangeslider/rangeslider.js", array("jquery"), "1.1", false);
+            "jquery.shortscorerangeslider.js", WP_PLUGIN_URL . "/wp-shortscore-rangeslider/rangeslider.js", array("jquery"), "1.2", false);
 
         wp_enqueue_script(
-            "jquery.shortscorerangeslider-init.js", WP_PLUGIN_URL . "/wp-shortscore-rangeslider/rangeslider-init.js", array("jquery"), "1.1", false);
+            "jquery.shortscorerangeslider-init.js", WP_PLUGIN_URL . "/wp-shortscore-rangeslider/rangeslider-init.js", array("jquery"), "1.2", false);
     }
 }
 
